64f38a0b32b0d6fcf22cc2b4a199668132c0306a
daawch <caique.rdg.0@gmail.com>
2022-04-07 16:33:52 -0300

    @daawch[css navbar]

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { AlertsService } from '../service/alerts.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  name = environment.name;
+  photo = environment.photo;
+  idUser = environment.id;
+  constructor(private route: Router,private alertService: AlertsService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout() {
+    environment.token = '';
+    environment.name = '';
+    environment.id = 0;
+    environment.photo = '';
+    this.route.navigate(['/login']);
+    this.alertService.showAlertWarning('Sessão encerrada');
   }
-
 }
