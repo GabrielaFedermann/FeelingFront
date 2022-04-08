eb2a733dd8378a7cff3a9e21a08e693bf27b0d69
daawch <caique.rdg.0@gmail.com>
2022-04-08 14:19:11 -0300

    @daawch[adding post edit/delete]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { PostDeleteComponent } from './delete/post-delete/post-delete.component';
 import { ThemeDeleteComponent } from './delete/theme-delete/theme-delete.component';
+import { PostEditComponent } from './edit/post-edit/post-edit.component';
 import { ThemeEditComponent } from './edit/theme-edit/theme-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { HomeComponent } from './home/home.component';
@@ -17,6 +19,8 @@ const routes: Routes = [
   {path: "theme", component: ThemeComponent},
   {path: "theme-edit/:id", component: ThemeEditComponent},
   {path: "theme-delete/:id", component: ThemeDeleteComponent},
+  {path: "post-edit/:id", component: PostEditComponent},
+  {path: "post-delete/:id", component: PostDeleteComponent}
 ];
 
 @NgModule({
